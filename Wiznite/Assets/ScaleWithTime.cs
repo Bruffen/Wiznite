536e7e0d31e6c0e836264e3316f986acfce3e562
Chuck <washinima99@gmail.com>
2018-12-29 17:31:53 +0000

    Nothing should be fucked

---
+++ Wiznite/Assets/ScaleWithTime.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWithTime : MonoBehaviour
+{
+    public float min, max, rate;
+
+    private Vector3 size;
+
+    // Use this for initialization
+    void Start()
+    {
+        size = new Vector3(max, 1, max);
+
+        this.transform.localScale = size;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        if (size.x > min)
+        {
+            size = this.transform.localScale;
+
+            size.x -= Time.deltaTime / rate;
+            size.z -= Time.deltaTime / rate;
+
+            this.transform.localScale = size;
+        }
+    }
+}
