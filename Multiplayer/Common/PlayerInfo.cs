b47903ea9231f089e29c9f5a4674fae14761e661
Chuck <washinima99@gmail.com>
2019-01-09 13:47:31 +0000

    Merge1

---
+++ Multiplayer/Common/PlayerInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    class PlayerInfo
+    {
+        public Guid Id { get; set; }
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+        public int Hp { get; set; }
+    }
+}
---
+++ Multiplayer/Common/SpellInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Common
+{
+    class SpellInfo
+    {
+        public float X { get; set; }
+        public float Y { get; set; }
+        public float Z { get; set; }
+
+        public float SpeedX { get; set; }
+        public float SpeedY { get; set; }
+        public float SpeedZ { get; set; }
+    }
+}
